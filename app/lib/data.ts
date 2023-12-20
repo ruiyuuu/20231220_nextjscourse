2bee5b6c0acfc269b458371051d349c784e0cd82
jasmine <jasmine.chen@mail.goldennet.com.tw>
2023-12-20 13:38:11 +0800

    newfile

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -28,7 +28,7 @@ export async function fetchRevenue() {
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
+    // throw new Error('Failed to fetch revenue data.');
   }
 }
 
@@ -48,7 +48,7 @@ export async function fetchLatestInvoices() {
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
+    // throw new Error('Failed to fetch the latest invoices.');
   }
 }
 
